d792bded51de3c61d821fbc119f74bb23ac30442
qinkaiyuan <qinkaiyuan94@163.com>
2020-12-08 23:06:11 +0800

    会话可以成功开启了

--- src/main/java/com/qingchi/base/modelVO/ChatVO.java
+++ src/main/java/com/qingchi/base/modelVO/ChatVO.java
@@ -2,6 +2,7 @@ package com.qingchi.base.modelVO;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.qingchi.base.constant.ChatType;
+import com.qingchi.base.constant.CommonStatus;
 import com.qingchi.base.model.chat.ChatDO;
 import com.qingchi.base.model.chat.ChatUserDO;
 import com.qingchi.base.model.chat.MessageDO;
@@ -86,7 +87,7 @@ public class ChatVO {
         this.lastContent = chatDO.getLastContent();
         this.unreadNum = 0;
         this.messages = new ArrayList<>();
-        this.needPayOpen = true;
+        this.needPayOpen = false;
     }
 
     //初始查询的时候为99
@@ -132,6 +133,9 @@ public class ChatVO {
         this.updateTime = chatUserDO.getUpdateTime();
         this.lastContent = chatUserDO.getLastContent();
         this.status = chatUserDO.getStatus();
+        if (this.status.equals(CommonStatus.waitOpen)) {
+            this.needPayOpen = true;
+        }
     }
 
     public ChatVO(ChatDO chatDO, ChatUserDO chatUserDO, boolean queryMsgFlag) {
